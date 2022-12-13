b571c67c4f454453342079fcd8ed15126444e6d8
Mickey254-ja <mickey.awino@student.moringaschool.com>
2022-12-13 11:04:42 +0300

    added titles

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function fetchMovies(){
         .then((response) => response.json()) //brings the response as a promise
         .then((data) =>{
             data.forEach((films) =>{
+   // Adds the film titles to the DOM in the form of a clickable menu
+   let li = document.createElement("li");
+   li.textContent = films.title;
+   li.addEventListener("click",
+   (e)=>{let buttonContent =
+     document.querySelector("button#buy-ticket")
+     buttonContent.textContent = "Buy Tickets"
+     let title =
+     document.getElementById("movie-title");
+        title.textContent =
+    films.title;             
